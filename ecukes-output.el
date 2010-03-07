e095ba0ae5a3a6af72d2ae37a20c9653083fe7af
Johan Andersson <johan.rejeep@gmail.com>
2010-03-07 15:32:41 +0000

    Offset doesn't need to be a global variable.

--- ecukes-output.el
+++ ecukes-output.el
@@ -1,19 +1,16 @@
 ;;; ecukes-output.el --- Helpers for printing feature stuff
 
-(defvar ecukes-output-offset 0
-  "Current offset (number of spaces).")
-
 (defconst ecukes-output-color-white 37
   "White color code.")
 
 (defun ecukes-output-intro (intro)
   "Outputs the feature intro"
-  (setq ecukes-output-offset 0)
-  (let ((header (ecukes-intro-header intro)))
-    (message (ecukes-output-white (concat "Feature: " header))))
-  (setq ecukes-output-offset 2)
-  (dolist (description (ecukes-intro-description intro))
-    (message (ecukes-output-white description))))
+  (let ((offset 0))
+    (let ((header (ecukes-intro-header intro)))
+      (message (ecukes-output-white (concat "Feature: " header))))
+    (setq offset 2)
+    (dolist (description (ecukes-intro-description intro))
+      (message (ecukes-output-white (concat (make-string offset 32) description))))))
 
 (defun ecukes-output-white (text)
   "Outputs TEXT in white."
@@ -21,7 +18,7 @@
 
 (defun ecukes-output-text (text color)
   "Outputs TEXT in COLOR."
-  (concat "\e[" (number-to-string color) "m" (make-string ecukes-output-offset 32) text "\e[0m"))
+  (concat "\e[" (number-to-string color) "m" text "\e[0m"))
 
 (provide 'ecukes-output)
 
