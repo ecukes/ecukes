b3cc92425aec6e26f564501518f892ca71ce22b5
Johan Andersson <johan.rejeep@gmail.com>
2010-03-07 16:37:21 +0000

    Added skeleton for run file.

--- ecukes-run.el
+++ ecukes-run.el
@@ -1,6 +1,25 @@
 ;;; ecukes-run.el --- Helpers for running stuff
 
+(add-to-list 'load-path (file-name-directory load-file-name))
+(require 'cl)
+(require 'ecukes-def)
+(require 'ecukes-dump)
+(require 'ecukes-output)
 
+(defun ecukes-run-and-print-background ()
+  ""
+
+  )
+
+(defun ecukes-run-background ()
+  ""
+
+  )
+
+(defun ecukes-run-and-print-scenario ()
+  ""
+
+  )
 
 (provide 'ecukes-run)
 
