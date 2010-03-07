03e271c0a5172f2f9f8d8d19ea23da7bf177f36b
Johan Andersson <johan.rejeep@gmail.com>
2010-03-07 15:38:08 +0000

    Offset is handled manually.

--- test/ecukes-output-test.el
+++ test/ecukes-output-test.el
@@ -1,7 +1,3 @@
 (ert-deftest ecukes-output-white ()
   (let ((text "text"))
     (should (equal "\e[37mtext\e[0m" (ecukes-output-white text)))))
-
-(ert-deftest ecukes-output-white-with-offset ()
-  (let ((text "text") (ecukes-output-offset 2))
-     (should (equal "\e[37m  text\e[0m" (ecukes-output-white text)))))
