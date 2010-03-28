4727953aaf44028ae6617db7f89523afed8e17dd
Johan Andersson <johan.rejeep@gmail.com>
2010-03-28 14:27:54 +0000

    Added run functions.

--- ecukes-run.el
+++ ecukes-run.el
@@ -1,19 +1,30 @@
-;;; ecukes-run.el ---
+;;; ecukes-run.el --- Run stuff
 
-(defun ecukes-run-scenario (scenario)
-  ""
+(defun ecukes-run-scenario (scenario fn)
+  "Runs all steps in SCENARIO, yielding each step and success flag."
+  (dolist (step (ecukes-scenario-steps scenario))
+    (let ((success (ecukes-run-step step)))
+      (funcall fn step success))))
 
-  )
-
-(defun ecukes-run-background (background)
-  ""
-
-  )
+(defun ecukes-run-background (background &optional fn)
+  "Runs all steps in BACKGROUND, yielding each step and success flag if FN."
+  (dolist (step (ecukes-background-steps background))
+    (let ((success (ecukes-run-step step)))
+      (if fn (funcall fn step success)))))
 
 (defun ecukes-run-step (step)
-  ""
-
-  )
+  "Runs STEP."
+  (condition-case err
+      (progn
+        (let* ((arg (ecukes-step-arg step))
+               (definition (ecukes-steps-find-definition step))
+               (fn (car definition))
+               (args (car (cdr definition))))
+          (if arg (add-to-list 'args arg t))
+          (apply fn args)))
+    (error
+     (setf (ecukes-step-err step) (error-message-string err))))
+  (not (ecukes-step-err step)))
 
 (provide 'ecukes-run)
 
