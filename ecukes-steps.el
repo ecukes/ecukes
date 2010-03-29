1a56ca365ca2fa1f25f220b6a3719dfdab3a1a4b
Johan Andersson <johan.rejeep@gmail.com>
2010-03-29 18:26:15 +0000

    Rename function.

--- ecukes-steps.el
+++ ecukes-steps.el
@@ -3,29 +3,29 @@
 (defvar ecukes-steps-definitions (make-hash-table :test 'equal)
   "Hash table containing all step definitions.")
 
-(defun ecukes-step-define (description fn)
+(defun ecukes-steps-define (description &optional fn)
   "Defines a step with DESCRIPTION and function FN.
  Do not use this function directly in step definitions. Use one of:
 `Given', `Then', `When', `And' or `But' instead."
   (puthash description fn ecukes-steps-definitions))
 
-(defalias 'Given 'ecukes-step-define
+(defalias 'Given 'ecukes-steps-define
   "Use this to put the system in a known state. Given what you say,
 the system should know that this is the case.")
 
-(defalias 'When 'ecukes-step-define
+(defalias 'When 'ecukes-steps-define
   "Describes the key action the user performs. When the user does
 something. Can be pressing a key, killing a buffer or selecting a region.")
 
-(defalias 'Then 'ecukes-step-define
+(defalias 'Then 'ecukes-steps-define
   "Observing the outcomes. Given something, then something else
 happens. Can be a buffer change or a font-size increasing.")
 
-(defalias 'And 'ecukes-step-define
+(defalias 'And 'ecukes-steps-define
   "If you have multiple given, when or then in a row. Then you can use
 And to make the text read more fluently.")
 
-(defalias 'But 'ecukes-step-define
+(defalias 'But 'ecukes-steps-define
   "If you have multiple given, when or then in a row. Then you can use
 But to make the text read more fluently.")
 
