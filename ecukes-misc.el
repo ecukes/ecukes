8d0e7a3d680e3112421b30454fb0500a09f4c5e8
Johan Andersson <johan.rejeep@gmail.com>
2010-03-31 14:07:33 +0000

    Hardcode debug tag, which outputs messages if t.

---
+++ ecukes-misc.el
@@ -0,0 +1,25 @@
+;;; ecukes-misc.el --- Miscellaneous helpers
+
+(defvar ecukes-messages '()
+  "List of messages that has been produced during the current scenario.")
+
+
+;; Print the message as usual, except add the message to the list `ecukes-messages'.
+(defadvice message (after message-after (format-string &rest args) activate)
+  (add-to-list 'ecukes-messages (apply 'format format-string args) t))
+
+;; Do not print anything.
+(defadvice message (around message-around (format-string &rest args) activate))
+
+(ad-activate 'message)
+
+(defun ecukes-message-advice (should-advice)
+  (if should-advice
+      (ad-enable-advice 'message 'around 'message-around)
+    (ad-disable-advice 'message 'around 'message-around))
+  (ad-update 'message))
+
+
+(provide 'ecukes-misc)
+
+;;; ecukes-misc.el ends here
