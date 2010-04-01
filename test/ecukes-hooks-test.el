1700198508ff73ef37ce7bdea9f252b0bb1dd93f
Johan Andersson <johan.rejeep@gmail.com>
2010-04-01 19:52:51 +0000

    Renamed tests.

--- test/ecukes-hooks-test.el
+++ test/ecukes-hooks-test.el
@@ -1,12 +1,12 @@
-(ert-deftest add-before-hook ()
+(ert-deftest hooks-add-before-hook ()
   (Before "before")
   (should (equal "before" (funcall (car ecukes-before-hooks)))))
 
-(ert-deftest add-after-hook ()
+(ert-deftest hooks-add-after-hook ()
   (After "after")
   (should (equal "after" (funcall (car ecukes-after-hooks)))))
 
-(ert-deftest before-hooks ()
+(ert-deftest hooks-add-before-hooks ()
   (Before
    (setq before-one 1))
   (Before
@@ -15,7 +15,7 @@
   (should (equal 1 before-one))
   (should (equal 2 before-two)))
 
-(ert-deftest after-hooks ()
+(ert-deftest hooks-add-after-hooks ()
   (After
    (setq after-one 1))
   (After
