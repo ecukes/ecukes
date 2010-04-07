3193219c52d1cc123a9749481251d6a493cd6e90
Johan Andersson <johan.rejeep@gmail.com>
2010-04-07 17:16:34 +0000

    Do not advice message to not print anything. Need better solution (added issue for it).

--- ecukes-message.el
+++ ecukes-message.el
@@ -1,4 +1,4 @@
-;;; ecukes-message.el --- Advising for message function
+;;; ecukes-message.el --- Advicing for message function
 
 (defvar ecukes-messages '()
   "List of messages that has been produced during the current scenario.")
@@ -7,19 +7,7 @@
 ;; Print the message as usual, except add the message to the list `ecukes-messages'.
 (defadvice message (after message-after (format-string &rest args) activate)
   (add-to-list 'ecukes-messages (apply 'format format-string args) t))
-
-;; Do not print anything.
-(defadvice message (around message-around (format-string &rest args) activate))
-
 (ad-activate 'message)
-
-(defun ecukes-message-advice (should-advice)
-  "Advise `message' to/not produce any ouput."
-  (if should-advice
-      (ad-enable-advice 'message 'around 'message-around)
-    (ad-disable-advice 'message 'around 'message-around))
-  (ad-update 'message))
-(ecukes-message-advice nil)
 
 
 (provide 'ecukes-message)
