343172b9b41685fa67662f1c12787f02970fcff9
Johan Andersson <johan.rejeep@gmail.com>
2010-04-07 22:01:03 +0000

    Clean up in run file.

--- ecukes-run.el
+++ ecukes-run.el
@@ -2,52 +2,24 @@
 
 (defun ecukes-run-features (features)
   "Runs all FEATURES."
-  (dolist (feature features)
-    (let ((background (ecukes-feature-background feature))
-          (scenarios (ecukes-feature-scenarios feature)))
-
-      ;; Print the intro, if any.
-      (let ((intro (ecukes-feature-intro feature)))
-        (if intro (ecukes-output-intro intro)))
-
-      ;; Run and output background
-      (if background
-          (ecukes-output-background
-           (ecukes-run-background
-            background
-            (lambda (step success)
-              (ecukes-stats-update-steps success)
-              (ecukes-output-step step success)))))
-
-      (dolist (scenario scenarios)
-        ;; Run before hooks
-        (ecukes-hooks-run-before)
-
-        ;; Turn on/off debug.
-        (let ((debug (member "debug" (ecukes-scenario-tags scenario))))
-          (ecukes-message-advice (not debug)))
-
-        ;; Run background
-        (if background (ecukes-run-background background))
-
-        ;; Run and output scenario
-        (let ((success-scenario t))
-          (ecukes-output-scenario
-           scenario
-           (ecukes-run-scenario
-            scenario
-            (lambda (step success-step)
-              (unless success-step (setq success-scenario nil))
-              (ecukes-stats-update-steps success-step)
-              (ecukes-output-step step success-step))))
-          (ecukes-stats-update-scenarios success-scenario))
-
-        ;; Clear the list of messages
-        (setq ecukes-messages '())
-
-        ;; Run after hooks
-        (ecukes-hooks-run-after))))
-  (ecukes-stats-print-summary))
+  (let ((background-run))
+    (dolist (feature features)
+      (let ((background (ecukes-feature-background feature))
+            (scenarios (ecukes-feature-scenarios feature))
+            (intro (ecukes-feature-intro feature)))
+        (if intro (ecukes-output-intro intro))
+        (dolist (scenario scenarios)
+          (ecukes-hooks-run-before)
+          (when background
+            (if background-run
+                (ecukes-run-background background)
+              (progn
+                (ecukes-run-and-print-background background)
+                (setq background-run t))))
+          (ecukes-run-and-print-scenario scenario)
+          (ecukes-message-clear)
+          (ecukes-hooks-run-after))))
+    (ecukes-stats-print-summary)))
 
 (defun ecukes-run-scenario (scenario fn)
   "Runs all steps in SCENARIO, yielding each step and success flag."
@@ -60,6 +32,28 @@
   (dolist (step (ecukes-background-steps background))
     (let ((success (ecukes-run-step step)))
       (if fn (funcall fn step success)))))
+
+(defun ecukes-run-and-print-background (background)
+  "Prints a background header and then runs all BACKGROUND's steps."
+  (ecukes-output-background
+   (ecukes-run-background
+    background
+    (lambda (step success)
+      (ecukes-stats-update-steps success)
+      (ecukes-output-step step success)))))
+
+(defun ecukes-run-and-print-scenario (scenario)
+  "Prints SCENARIO's header and then runs all SCENARIO's steps."
+  (let ((success-scenario t))
+    (ecukes-output-scenario
+     scenario
+     (ecukes-run-scenario
+      scenario
+      (lambda (step success-step)
+        (unless success-step (setq success-scenario nil))
+        (ecukes-stats-update-steps success-step)
+        (ecukes-output-step step success-step))))
+    (ecukes-stats-update-scenarios success-scenario)))
 
 (defun ecukes-run-step (step)
   "Runs STEP.
