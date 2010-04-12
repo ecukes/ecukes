182e08a6bc75d45288554bdd371ebf27b52affee
Johan Andersson <johan.rejeep@gmail.com>
2010-04-12 20:27:26 +0000

    Fix message bug.

--- ecukes-message.el
+++ ecukes-message.el
@@ -4,25 +4,34 @@
   "List of messages that has been produced during the current scenario.")
 
 
-;; Print the message as usual, except add the message to the list `ecukes-messages'.
-(defadvice message (after message-add-history (format-string &rest args) activate)
-  (add-to-list 'ecukes-message-history (apply 'format format-string args) t))
-(ad-activate 'message)
+(defun ecukes-message-disable-advice ()
+  "Disables the message history advice."
+  (ad-disable-advice 'message 'after 'message-add-history)
+  (ad-update 'message))
 
+(defun ecukes-message-enable-advice ()
+  "Enables the message history advice."
+  (ad-enable-advice 'message 'after 'message-add-history)
+  (ad-activate 'message)
+  (ad-update 'message))
 
 (defmacro ecukes-message-no-history (&rest body)
-  "Turns off the history advice, evaluates BODY and then turns it back on.
-Useful to print a message without storing it to the history."
+  "Turns off the history advice, evaluates BODY and then turns it back
+on. Useful to print a message without storing it to the history."
   `(progn
-     (ad-disable-advice 'message 'after 'message-add-history)
-     (ad-update 'message)
+     (ecukes-message-disable-advice)
      ,@body
-     (ad-enable-advice 'message 'after 'message-add-history)
-     (ad-update 'message)))
+     (ecukes-message-enable-advice)))
 
 (defun ecukes-message-clear ()
   "Clears the message history list."
   (setq ecukes-message-history '()))
+
+
+;; Print the message as usual, except add the message to the list `ecukes-messages'.
+(defadvice message (after message-add-history (format-string &rest args) activate)
+  (add-to-list 'ecukes-message-history (apply 'format format-string args) t))
+(ecukes-message-enable-advice)
 
 
 (provide 'ecukes-message)
