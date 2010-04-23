699b06b97daf4d3782eb090a2ac5898f6fcda15d
Johan Andersson <johan.rejeep@gmail.com>
2010-04-23 10:43:14 +0000

    Updating startup stuff.

--- ecukes-startup.el
+++ ecukes-startup.el
@@ -1,52 +1,52 @@
 ;;; ecukes-startup.el --- TODO
 
-;; TODO: Rename init functions!
-;; TODO: Test functions!
 
-;; TODO: Need to group?
-(defconst ecukes-init-root-regex "\\(?:^[[:alpha:]]:/$\\|^/$\\)"
+(defconst ecukes-root-regex "\\(?:^[[:alpha:]]:/$\\|^/$\\)"
   "Regular expression matching a file system root.")
 
 
-(defun ecukes-init-feature-files (features)
-  "Returns a list of feature files given user input.
-FEATURES is a list where an item can be either a .feature file or a
-directory. If a directory, all .feature files in that directory are included."
-  (let ((feature-files))
-    (dolist (feature features)
-      (if (file-directory-p feature)
-          (dolist (feature-file (directory-files feature t "\\.feature$"))
-            (add-to-list 'feature-files feature-file))
-        (if (file-exists-p feature)
-            (add-to-list 'feature-files (expand-file-name feature)))))
-    feature-files))
+(defun ecukes-feature-files (arguments)
+  "Given ecukes arguments, return a list of all feature files.
 
-(defun ecukes-init-load-project (argv)
-  "Loads all project Ecukes files."
-  (let ((features-root (ecukes-init-features-root (car argv))))
+ARGUMENTS can be either a list of .feature files or a directory. If a
+directory, all .feature files in that directory will be included."
+  (when arguments
+    (let ((feature-files))
+      (dolist (argument arguments)
+        (when (file-exists-p argument)
+          (if (file-directory-p argument)
+              (dolist (feature-file (directory-files argument t "\\.feature$"))
+                (add-to-list 'feature-files (expand-file-name feature-file) t))
+            (add-to-list 'feature-files (expand-file-name argument) t))))
+      feature-files)))
+
+(defun ecukes-load-project (arguments)
+  "Loads all Ecukes files in project."
+  (let* ((project-file (car arguments))
+         (features-root (ecukes-features-root project-file)))
     (cond (features-root
            (load (expand-file-name "support.el" features-root))
            (dolist (step (directory-files (expand-file-name "step-definitions" features-root) t "-steps\\.el$"))
              (load step)))
-          (t (error "Could not find any features root")))))
+          (t (ecukes-output-red "Could not find features root")))))
 
-(defun ecukes-init-features-root (dir)
-  "Returns project features root directory."
-  (let ((project-root (ecukes-init-project-root dir)))
+(defun ecukes-features-root (file)
+  "Returns the project features directory from FILE in project."
+  (let ((project-root (ecukes-project-root file)))
     (if project-root (expand-file-name "features" project-root))))
 
-(defun ecukes-init-project-root (dir)
-  "Returns project root directory."
-  (if (file-regular-p dir)
-      (ecukes-init-project-root (file-name-directory dir))
-    (if (file-directory-p (expand-file-name "features" dir))
-        dir
-      (let ((new-dir (expand-file-name (file-name-as-directory "..") dir)))
-        (unless (string-match-p ecukes-init-root-regex dir)
-          (ecukes-init-project-root new-dir))))))
+(defun ecukes-project-root (file)
+  "Returns the project root directory from FILE in project."
+  (if (file-regular-p file)
+      (ecukes-project-root (file-name-directory dir))
+    (if (file-directory-p (expand-file-name "features" file))
+        file
+      (let ((new-dir (expand-file-name (file-name-as-directory "..") file)))
+        (unless (string-match-p ecukes-root-regex file)
+          (ecukes-project-root new-dir))))))
 
 (defun ecukes-options-p ()
-  "TODO"
+  "Returns t if any options was sent to ecukes, nil otherwise."
   (let ((opts (mapcar (lambda (e) (car e)) command-switch-alist))
         (match) (arguments (copy-sequence argv)))
     (while (and (not match) arguments)
--- test/ecukes-startup-test.el
+++ test/ecukes-startup-test.el
@@ -1,29 +1,49 @@
-;; TODO: Test better!!!
+(ert-deftest startup-root-regex-unix ()
+  (should (string-match-p ecukes-root-regex "/")))
 
-(ert-deftest init-root-regex-unix ()
-  (should (string-match-p ecukes-init-root-regex "/")))
+(ert-deftest startup-root-regex-win ()
+  (should (string-match-p ecukes-root-regex "c:/"))
+  (should (string-match-p ecukes-root-regex "D:/")))
 
-(ert-deftest init-root-regex-win ()
-  (should (string-match-p ecukes-init-root-regex "c:/")))
-
-(ert-deftest init-pass-no-feature-file ()
-  (let ((feature-files (ecukes-init-feature-files (list))))
+(ert-deftest startup-feature-files-no-arguments ()
+  (let ((feature-files (ecukes-feature-files nil)))
     (should-not feature-files)))
 
-(ert-deftest init-pass-feature-file-that-does-no-exist ()
-  (let ((feature-files (ecukes-init-feature-files (list "does not exist"))))
+(ert-deftest startup-feature-files-single-argument-does-not-exist ()
+  (let ((feature-files (ecukes-feature-files '("does-not.exist"))))
     (should-not feature-files)))
 
-(ert-deftest init-pass-single-feature-file ()
+(ert-deftest startup-feature-files-multiple-arguments-does-not-exist ()
+  (let ((feature-files (ecukes-feature-files '("does-not.exist" "non-existing.file"))))
+    (should-not feature-files)))
+
+(ert-deftest startup-feature-files-valid-single-feature-file ()
   (with-mock
     (stub file-exists-p => t)
-    (let ((feature-files (ecukes-init-feature-files (list "test.feature"))))
+    (stub file-directory-p => nil)
+    (let ((feature-files (ecukes-feature-files '("test.feature"))))
       (should (string-match-p "test\\.feature$" (car feature-files))))))
 
-(ert-deftest init-pass-directory-of-features ()
+(ert-deftest startup-feature-files-valid-multiple-feature-files ()
+  (with-mock
+    (stub file-exists-p => t)
+    (stub file-directory-p => nil)
+    (let ((feature-files (ecukes-feature-files '("test1.feature" "test2.feature"))))
+      (should (string-match-p "test1\\.feature$" (car feature-files)))
+      (should (string-match-p "test2\\.feature$" (cadr feature-files))))))
+
+(ert-deftest startup-pass-directory-of-features ()
   (with-mock
     (stub file-directory-p => t)
     (stub directory-files => (list "test1.feature" "test2.feature"))
-    (let ((feature-files (ecukes-init-feature-files (list "features"))))
-      (should (string-match-p "test2\\.feature$" (car feature-files)))
-      (should (string-match-p "test1\\.feature$" (cadr feature-files))))))
+    (let ((feature-files (ecukes-feature-files '("features"))))
+      (should (string-match-p "test1\\.feature$" (car feature-files)))
+      (should (string-match-p "test2\\.feature$" (cadr feature-files))))))
+
+(ert-deftest startup-load-non-existing-project ()
+  (with-mock
+   (stub ecukes-features-root => nil)
+   (track-output
+    (quiet-message (ecukes-load-project nil))
+    (should
+     (member (ecukes-color-red "Could not find features root") message-output)))))
