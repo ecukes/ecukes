fb5805933623237487204f473c3878608eb39dbe
Johan Andersson <johan.rejeep@gmail.com>
2010-10-17 16:57:25 +0200

    Expand helper in support directory.

--- ecukes-startup.el
+++ ecukes-startup.el
@@ -28,7 +28,7 @@ directory, all .feature files in that directory will be included."
     (cond (features-root
            (load (expand-file-name "env.el" support-dir))
            (dolist (helper (directory-files support-dir nil "[^env.el]\\.*.el"))
-             (load helper))
+             (load (expand-file-name helper support-dir)))
            (dolist (step (directory-files (expand-file-name "step-definitions" features-root) t "-steps\\.el$"))
              (load step)))
           (t (ecukes-output-red "Could not find features root")))))
