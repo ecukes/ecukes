00dd63d93c34e36a1b0f42077b9f3de3e0e4b104
Johan Andersson <johan.rejeep@gmail.com>
2011-01-12 10:16:31 +0100

    Refactoring definitions.

--- ecukes-def.el
+++ ecukes-def.el
@@ -1,57 +1,30 @@
 ;;; ecukes-def.el --- Data structure definitions
 
-;; A feature, which is the top level structure for a feature file.
-;;
-;;   intro      - The feature introduction if any.
-;;   background - The feature background if any.
-;;   scenarios  - A list of all scenarios in the feature.
-(defstruct ecukes-feature intro background scenarios)
 
+(defstruct ecukes-feature
+  "A feature is the top level structure for a feature file."
+  intro background scenarios)
 
-;; Feature introduction. Introduction is optional, but is conventionally included.
-;;
-;; header      - One line description of feature.
-;; description - Conventionally consists of three lines, like this:
-;;                 In order ...
-;;                 As a ...
-;;                 I want ...
-;;               It could however consist of fewer or more lines. For example:
-;;                 In order ...
-;;                 A ... wants ...
-;;
-;;               The description is a list where each item is a line.
-(defstruct ecukes-intro header description)
+(defstruct ecukes-intro
+  "A feature introduction is a description of a feature. It is
+optional, but is conventionally included."
+  header description)
 
+(defstruct ecukes-background
+  "A feature background is a few steps that are run before each scenario."
+  steps)
 
-;; Feature background, which a feature optionally can include.
-;;
-;; steps - A list of all steps in the background.
-(defstruct ecukes-background steps)
+(defstruct ecukes-scenario
+  "A feature scenario is a scenario that is built up by steps."
+  name steps tags)
 
+(defstruct ecukes-step
+  "A step is some kind of action."
+  name arg type err)
 
-;; A feature scenario. Each feature can consist of many scenarios.
-;;
-;; name  - The name of the scenario. For example: Switch to *Messages* buffer.
-;; steps - A list of all steps in the scenario.
-;; tags  - A list of all tags in the scenario.
-(defstruct ecukes-scenario name steps tags)
-
-
-;; A step found either in a background or scenario.
-;;
-;; name - Name of the step. For example: Given I press "C-h e".
-;; type - The type of the step. Can be 'regular, 'py-string or 'table.
-;; arg  - If steps is a table or py-string step. arg is either the
-;;        py-string or the table. If a regular step, this is nil.
-;; err  - When running the step. If it fails, this contains the error message.
-(defstruct ecukes-step name arg type err)
-
-
-;; A step definition.
-;;
-;; fn   - The step definition function. Can be either a lambda or a symbol.
-;; args - The argument from the regular expression match groupings.
-(defstruct ecukes-step-def fn args)
+(defstruct ecukes-step-def
+  "A step definition"
+  fn args)
 
 
 (provide 'ecukes-def)
