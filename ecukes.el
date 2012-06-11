4791ca280f6cc5caf88ee5ba54f91af28bf7bcea
Joel McCracken <mccracken.joel@gmail.com>
2012-06-10 22:27:25 -0400

    Added bash script to help manage the way emacs is run
    The ecukes command is now a bash script.
    ecukes was renamed to ecukes.el, which is now loaded by the script.
    You may run ecukes in a graphical way by running it with the argument --graphical
    That should clearly be renamed, but I am currently unimaginative.
    You may also specify the argument that ecukes uses to run emacs via
    the shell variable ECUKES_EMACS; set it to the path of the emacs you
    would like it to run.

---
+++ ecukes.el
@@ -0,0 +1,162 @@
+#!/usr/bin/env emacs --script
+;;; ecukes --- Cucumber for Emacs
+
+;; Copyright (C) 2010 Johan Andersson
+
+;; Author: Johan Andersson <johan.rejeep@gmail.com>
+;; Maintainer: Johan Andersson <johan.rejeep@gmail.com>
+;; Version: 0.1.0
+;; Keywords: testing, cucumber
+;; URL: http://github.com/rejeep/ecukes
+
+;; This file is NOT part of GNU Emacs.
+
+;;; License:
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation; either version 3, or (at your option)
+;; any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with GNU Emacs; see the file COPYING.  If not, write to the
+;; Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+;; Boston, MA 02110-1301, USA.
+
+;;; Commentary:
+
+;; See README
+
+;;; Code:
+
+(setq debug-on-error t)
+(setq debug-on-entry t)
+
+(defvar ecukes-path
+  (file-name-directory load-file-name)
+  "Path to ecukes.")
+
+(defvar ecukes-vendor-path
+  (expand-file-name "vendor" ecukes-path)
+  "Path to ecukes vendor.")
+
+(add-to-list 'load-path ecukes-path)
+(add-to-list 'load-path ecukes-vendor-path)
+
+(eval-when-compile
+  (require 'cl))
+
+(require 'ansi)
+(require 'ecukes-template)
+(require 'ecukes-new)
+(require 'ecukes-def)
+(require 'ecukes-startup)
+(require 'ecukes-parse)
+(require 'ecukes-steps)
+(require 'ecukes-run)
+(require 'ecukes-print)
+(require 'ecukes-hooks)
+(require 'ecukes-stats)
+
+(add-to-list 'command-switch-alist '("--new" . ecukes-new-handler))
+
+(when (ecukes-startup-run-p)
+
+  (defun run-step (step &optional print background)
+    "Run STEP, including increasing counters."
+    (if previous-step-success
+        (cond
+         ((ecukes-run-step step)
+          (unless background (ecukes-stats-step-pass))
+          (if print
+              (ecukes-print-step-success step)))
+         (t
+          (unless background (ecukes-stats-step-fail))
+          (setq step-has-failed t)
+          (if print
+              (ecukes-print-step-failure step))
+          (setq previous-step-success nil)))
+      (ecukes-stats-step-skip)
+      (if print
+          (ecukes-print-step-pending step))))
+
+  (let ((feature-files (ecukes-startup-features argv)))
+    (cond (feature-files
+
+           (ecukes-startup-load)
+           
+           (ecukes-hooks-run-setup)
+
+           (dolist (feature-file feature-files)
+             (let* ((feature (ecukes-parse-feature feature-file))
+                    (background (ecukes-feature-background feature))
+                    (scenarios (ecukes-feature-scenarios feature))
+                    (steps
+                     (apply
+                      'append
+                      (if background (ecukes-background-steps background))
+                      (mapcar 'ecukes-scenario-steps scenarios)))
+                    (undefined (ecukes-steps-undefined steps)))
+
+               (setq step-has-failed nil)
+               (setq previous-step-success t)
+               (setq background-runned nil)
+
+               (cond (undefined
+                      (ecukes-print-undefined-steps undefined))
+                     ((let ((intro (ecukes-feature-intro feature)))
+                        (ecukes-print-intro intro)
+
+                        (when background
+                          (ecukes-print-background-header)
+                          (dolist (step (ecukes-background-steps background))
+                            (run-step step t))
+                          (setq background-runned t))
+
+                        (dolist (scenario scenarios)
+                          (ecukes-hooks-run-before)
+                          
+                          (when background
+                            (dolist (step (ecukes-background-steps background))
+                              (run-step step nil t)))
+                          
+                          (ecukes-print-newline)
+                          (ecukes-print-scenario-header scenario)
+                          (dolist (step (ecukes-scenario-steps scenario))
+                            (run-step step t))
+                          
+                          (if step-has-failed
+                              (ecukes-stats-scenario-fail)
+                            (ecukes-stats-scenario-pass))
+                          
+                          (setq previous-step-success t)
+                          (setq step-has-failed nil)
+                          
+                          (ecukes-hooks-run-after)))))))
+           
+           (ecukes-hooks-run-teardown)
+
+           (ecukes-stats-print-summary))
+          (t
+           (ecukes-print-message
+            (ansi-red "You did not provide any features to run"))))))
+
+
+(when (boundp 'ecukes-tmp-file-target)
+  (with-temp-buffer
+    (insert-buffer "*Messages*")
+    ;; ecukes-tmp-file-target needs to get set from somewhere else
+    (write-file ecukes-tmp-file-target))
+
+  ;; kill emacs needs to happen because when ecukes-tmp-file-target
+  ;; is set, emacs is running as graphical and -q
+  (kill-emacs))
+
+
+
+;;; ecukes.el ends here
