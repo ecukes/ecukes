4fb4d81363e5d9f7fa8ec73f875c3c0686537769
Dave Paroulek <upgradingdave@gmail.com>
2012-08-24 15:43:23 -0400

    Setup directory paths relative to current buffer each time ecukes is run
    When the ecukes shell script is run from project directory, defvar works
    fine. But when run from inside emacs, ecukes is loaded on startup and was
    setting the paths relative to where emacs was started. So, unless careful
    to start emacs from project directory, the paths were not correct.
    
    By changing defvars to defuns in ecukes-startup, the paths are calculated
    each time ecukesmis run. Paths are then are then set relative to the
    current buffer and this allows ecukes to be run inside emacs.

--- ecukes-startup.el
+++ ecukes-startup.el
@@ -1,25 +1,25 @@
 ;;; ecukes-startup.el --- Helper functions when starting up Ecukes
 
 
-(defvar ecukes-startup-project-path
-  (directory-file-name (expand-file-name default-directory))
-  "Path to the project.")
+(defun ecukes-startup-project-path ()
+  "Path to the project."
+  (directory-file-name (expand-file-name default-directory)))
 
-(defvar ecukes-startup-project-name
-  (file-name-nondirectory ecukes-startup-project-path)
-  "Name of the project.")
+(defun ecukes-startup-project-name ()
+  "Name of the project."
+  (file-name-nondirectory (ecukes-startup-project-path)))
 
-(defvar ecukes-startup-features-path
-  (expand-file-name "features" ecukes-startup-project-path)
-  "Path to the project features directory.")
+(defun ecukes-startup-features-path ()
+  "Path to the project features directory."
+  (expand-file-name "features" (ecukes-startup-project-path)))
 
-(defvar ecukes-startup-support-path
-  (expand-file-name "support" ecukes-startup-features-path)
-  "Path to the features support directory.")
+(defun ecukes-startup-support-path ()
+  "Path to the features support directory."
+  (expand-file-name "support" (ecukes-startup-features-path)))
 
-(defvar ecukes-startup-step-definitions-path
-  (expand-file-name "step-definitions" ecukes-startup-features-path)
-  "Path to the features step definitions directory.")
+(defun ecukes-startup-step-definitions-path ()
+  "Path to the features step definitions directory."
+  (expand-file-name "step-definitions" (ecukes-startup-features-path)))
 
 
 (defun ecukes-startup-load ()
@@ -29,8 +29,8 @@
 
 (defun ecukes-startup-load-support ()
   "Load project support files."
-  (load (expand-file-name "env.el" ecukes-startup-support-path) nil t)
-  (let ((support-files (remove "env.el" (directory-files ecukes-startup-support-path t "\\.el$"))))
+  (load (expand-file-name "env.el" (ecukes-startup-support-path)) nil t)
+  (let ((support-files (remove "env.el" (directory-files (ecukes-startup-support-path) t "\\.el$"))))
     (mapc
      (lambda (support-file)
        (load support-file nil t))
@@ -38,7 +38,7 @@
 
 (defun ecukes-startup-load-step-definitions ()
   "Load project step definition files."
-  (let ((step-definition-files (directory-files ecukes-startup-step-definitions-path t "-steps\\.el$")))
+  (let ((step-definition-files (directory-files (ecukes-startup-step-definitions-path) t "-steps\\.el$")))
     (mapc
      (lambda (step-definition-file)
        (load step-definition-file nil t))
@@ -51,7 +51,7 @@
         (if (file-directory-p file)
             (directory-files file t "\\.feature$")
           files))
-    (ecukes-startup-features (list ecukes-startup-features-path))))
+    (ecukes-startup-features (list (ecukes-startup-features-path)))))
 
 (defun ecukes-startup-run-p ()
   "Should we run feature or was there an option."
