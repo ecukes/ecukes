8bad2ed957082ce6012789e36021a2067fc39501
Johan Andersson <johan.rejeep@gmail.com>
2012-11-11 10:56:48 +0100

    Move header comments to ecukes file.

--- ecukes.el
+++ ecukes.el
@@ -1,38 +1,4 @@
-#!/usr/bin/env emacs --script
-;;; ecukes --- Cucumber for Emacs
-
-;; Copyright (C) 2010 Johan Andersson
-
-;; Author: Johan Andersson <johan.rejeep@gmail.com>
-;; Maintainer: Johan Andersson <johan.rejeep@gmail.com>
-;; Version: 0.2.1
-;; Keywords: testing, cucumber
-;; URL: http://github.com/rejeep/ecukes
-
-;; This file is NOT part of GNU Emacs.
-
-;;; License:
-
-;; This program is free software; you can redistribute it and/or modify
-;; it under the terms of the GNU General Public License as published by
-;; the Free Software Foundation; either version 3, or (at your option)
-;; any later version.
-
-;; This program is distributed in the hope that it will be useful,
-;; but WITHOUT ANY WARRANTY; without even the implied warranty of
-;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-;; GNU General Public License for more details.
-
-;; You should have received a copy of the GNU General Public License
-;; along with GNU Emacs; see the file COPYING.  If not, write to the
-;; Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
-;; Boston, MA 02110-1301, USA.
-
-;;; Commentary:
-
-;; See README
-
-;;; Code:
+;;; ecukes.el --- Cucumber for Emacs
 
 (setq debug-on-error t)
 (setq debug-on-entry t)
