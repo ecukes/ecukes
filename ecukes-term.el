0a95724b99ebb09fde36fb140ba9cc772eed324b
Johan Andersson <johan.rejeep@gmail.com>
2012-11-11 11:08:56 +0100

    Remove some comments.

--- ecukes-term.el
+++ ecukes-term.el
@@ -1,38 +1,4 @@
-#!/usr/bin/env emacs --script
-;;; ecukes --- Cucumber for Emacs
-
-;; Copyright (C) 2010 Johan Andersson
-
-;; Author: Johan Andersson <johan.rejeep@gmail.com>
-;; Maintainer: Johan Andersson <johan.rejeep@gmail.com>
-;; Version: 0.2.0
-;; Keywords: testing, cucumber
-;; URL: http://github.com/rejeep/ecukes
-
-;; This file is NOT part of GNU Emacs.
-
-;;; License:
-
-;; This program is free software; you can redistribute it and/or modify
-;; it under the terms of the GNU General Public License as published by
-;; the Free Software Foundation; either version 3, or (at your option)
-;; any later version.
-
-;; This program is distributed in the hope that it will be useful,
-;; but WITHOUT ANY WARRANTY; without even the implied warranty of
-;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-;; GNU General Public License for more details.
-
-;; You should have received a copy of the GNU General Public License
-;; along with GNU Emacs; see the file COPYING.  If not, write to the
-;; Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
-;; Boston, MA 02110-1301, USA.
-
-;;; Commentary:
-
-;; See README
-
-;;; Code:
+;;; ecukes-term.el --- Cucumber for Emacs
 
 (defvar ecukes-path
   (file-name-directory load-file-name)
@@ -62,6 +28,4 @@
   ;; is set, emacs is running as graphical and -q
   (kill-emacs))
 
-
-
-;;; ecukes.el ends here
+;;; ecukes-term.el ends here
