d1030564a45b2d9b9cd8dc509e86258f7f2bee6f
Johan Andersson <johan.rejeep@gmail.com>
2012-11-13 20:45:23 +0100

    Send correct feature files to run when running Ecukes from terminal.

--- ecukes-term.el
+++ ecukes-term.el
@@ -11,8 +11,14 @@
 (require 'ecukes-setup)
 
 (ecukes-setup)
-;; Send what files to run...!
-(ecukes-run)
+
+(let ((feature-files
+       (progn
+         (or argv (setq argv (list "features")))
+         (if (file-directory-p (car argv))
+             (directory-files (ecukes-project-features-path) t "\\.feature$")
+           argv))))
+  (ecukes-run feature-files))
 
 (ecukes-quit
  (if (> ecukes-stats-steps-failed 0) 1 0))
