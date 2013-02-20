1e477ef20aff9f28e5a43c188dd7c086d9f04f5d
Takafumi Arakaki <aka.tkf@gmail.com>
2013-02-20 16:40:06 +0100

    Add --with-doc/--with-file options

--- ecukes.el
+++ ecukes.el
@@ -74,6 +74,15 @@ Include docstring when WITH-DOC is non-nil."
        (princ "\n")))
    ecukes-steps-definitions))
 
+(defun ecukes-cli-print-steps ()
+  (let ((has (lambda (flag)
+               (when (member flag command-line-args-left)
+                 (setq command-line-args-left
+                       (delete flag command-line-args-left))
+                 t))))
+    (ecukes-print-steps (funcall has "--with-doc")
+                        (funcall has "--with-file"))))
+
 (provide 'ecukes)
 
 ;;; ecukes.el ends here
