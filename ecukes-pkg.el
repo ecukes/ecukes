3ee5464d86ba47fceea5c1d6e55a6209e127439d
Johan Andersson <johan.rejeep@gmail.com>
2013-10-29 17:55:12 +0100

    Release v0.6.8.

--- ecukes-pkg.el
+++ ecukes-pkg.el
@@ -1,3 +1,3 @@
-(define-package "ecukes" "0.6.0"
+(define-package "ecukes" "0.6.8"
   "Cucumber for Emacs."
-  '((f "0.7.1") (s "1.6.2") (dash "2.1.0") (ansi "0.3.0") (espuds "0.2.2") (commander "0.4.0")))
+  '((f "0.11.0") (s "1.8.0") (dash "2.2.0") (ansi "0.3.0") (espuds "0.2.2") (commander "0.5.1")))
